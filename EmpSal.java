addc48be4b2fb684c00a329b01c6d863bdee565a
Vickey <118042499+VickeyDounde@users.noreply.github.com>
2024-01-15 17:51:47 +0530

    Add files via upload

---
+++ EmpSal.java
@@ -0,0 +1,20 @@
+public class EmpSal
+{
+    public static void main(String args[])
+    {
+          float sal=3000;
+          float pf;
+          if(sal>=15000)
+          {
+             pf = sal*.12F;
+             sal = sal-pf;
+             System.out.println("Total PF Deduction is"+pf); 
+          } 
+
+         System.out.println("Total Salary is "+sal);
+
+    }
+
+
+
+}
